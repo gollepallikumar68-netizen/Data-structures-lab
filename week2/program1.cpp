a9346f3786dddb259dbbe5723555b4af6738c1a0
gollepallikumar68-netizen <gollepallikumar68@gmail.com>
2026-02-16 18:46:11 +0530

    program1.cpp

---
+++ week2/program1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[10],i;
+	cout<<"Enter array elements:";
+	for(i=0;i<10;i++)
+	cin>>a[i];
+	cout<<"\n array elements:";
+	for(i=0;i<10;i++)
+	cout<<a[i];
+	return 0;
+}
